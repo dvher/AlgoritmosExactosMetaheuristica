61edc2d9080fd32e8ab731098aefccf9bcb2bbe1
Ketbome <pims.2711@gmail.com>
2023-05-08 23:32:36 -0400

    feat: :fire: Estocastico Listo y determinista arreglado un problema

--- Tarea2/read.py
+++ Tarea2/read.py
@@ -23,38 +23,46 @@ def read_file(file_name: str) -> Tuple[List[int], List[List[int]]]:
 
 
 def greedy_determinista(num_uavs, uavs, t_espera):
+    ord_actual = 0 # En que valor del orden va
     orden = sorted(range(num_uavs), key=lambda i: uavs[i][1])  # Ordena los UAV por tiempo preferente
+    print(orden)
     tiempos_aterrizaje = [0] * num_uavs # Hace un arreglo de 0ros de tamaño num_uavs
     costo = 0
 
     for i in orden:
-        tiempo_aterrizaje = max(uavs[i][0], tiempos_aterrizaje[i - 1] + t_espera[i - 1][i] if i > 0 else 0)
+        tiempo_aterrizaje = max(uavs[i][0], tiempos_aterrizaje[orden[ord_actual - 1]] + t_espera[orden[ord_actual - 1]][i] if i > 0 else 0)
         tiempos_aterrizaje[i] = tiempo_aterrizaje
         if tiempo_aterrizaje < uavs[i][1]:
             costo += (uavs[i][1] - tiempo_aterrizaje) * 2  # Costo adicional por estar por debajo del tiempo preferente
         else:
             costo += abs(tiempo_aterrizaje - uavs[i][1])
+        ord_actual += 1
 
 
     return costo, tiempos_aterrizaje
 
 def greedy_estocastico(num_uavs, uavs, t_espera, seed):
+    ord_actual = 0 # En que valor del orden va
     random.seed(seed)
     orden = sorted(range(num_uavs), key=lambda i: uavs[i][1])  # Ordena los UAV por tiempo preferente
-    tiempos_aterrizaje = [0] * num_uavs
+    tiempos_aterrizaje = [0] * num_uavs # Hace un arreglo de 0ros de tamaño num_uavs
     costo = 0
 
     for i in orden:
-        rango_tiempo = list(range(uavs[i][0], uavs[i][2] + 1))
+        t_sig_aterrizaje = max(uavs[i][0], tiempos_aterrizaje[orden[ord_actual - 1]] + t_espera[orden[ord_actual - 1]][i] if i > 0 else 0)
+        rango_tiempo = list(range(t_sig_aterrizaje, t_sig_aterrizaje + 50)) # Aca para hacerlo estocastico que elija entre un rango de tiempos desde el que puede aterrizar
         tiempo_aterrizaje = random.choice(rango_tiempo)
         tiempos_aterrizaje[i] = tiempo_aterrizaje
         if tiempo_aterrizaje < uavs[i][1]:
             costo += (uavs[i][1] - tiempo_aterrizaje) * 2  # Costo adicional por estar por debajo del tiempo preferente
+        elif tiempo_aterrizaje > uavs[i][2]:
+            costo += (tiempo_aterrizaje - uavs[i][2]) * 10 # Muy costoso solucion
         else:
             costo += abs(tiempo_aterrizaje - uavs[i][1])
+        ord_actual += 1
+
 
     return costo, tiempos_aterrizaje
-
 
 
 
