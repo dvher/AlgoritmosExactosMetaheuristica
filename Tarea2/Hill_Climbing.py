02970b46923302dfed980784b1ebb73d00195049
Ketbome <pims.2711@gmail.com>
2023-05-16 14:45:24 -0400

    docs: :memo: Agregado de documentacion

--- Tarea2/Hill_Climbing.py
+++ Tarea2/Hill_Climbing.py
@@ -7,17 +7,17 @@ def hill_climbing(num_uavs: int, uavs: List[List[int]], t_espera: List[List[int]
     for n, i in enumerate(orden):
         if tiempos_aterrizaje_actual[i] < uavs[i][1]:
             # Se ve si se puede mejorar el tiempo acercandolo a el tiempo preferente para bajar el costo
-            if n >= 14:
+            if n >= 14: # en el caso de ser el último al ser menor el tiempo de aterrizaje que el preferente, este puede llegar al preferente dado que no hay naves que deban aterrizar despues de él
                 nuevo_tiempo_aterrizaje = uavs[i][1]
                 nuevo_costo = 0
-                if nuevo_costo < costo_orden[i]:
+                if nuevo_costo < costo_orden[i]: # Si el costo nuevo es menor se agrega y tiempos de aterrizaje recien se actualiza
                     costo_actual += nuevo_costo - costo_orden[i]
                     costo_orden[i] = nuevo_costo
                     tiempos_aterrizaje_actual[i] = nuevo_tiempo_aterrizaje
-            elif tiempos_aterrizaje_actual[orden[n + 1]] - t_espera[orden[n + 1]][i] < uavs[i][1]:
-                nuevo_tiempo_aterrizaje = tiempos_aterrizaje_actual[orden[n + 1]] - t_espera[orden[n + 1]][i]
+            elif tiempos_aterrizaje_actual[orden[n + 1]] - t_espera[orden[n]][i] < uavs[i][1]: # Se ve el tiempo de aterrizaje al siguiente y se le resta la espera
+                nuevo_tiempo_aterrizaje = tiempos_aterrizaje_actual[orden[n + 1]] - t_espera[orden[n]][i]
                 nuevo_costo = uavs[i][1] - nuevo_tiempo_aterrizaje
-                if nuevo_costo < costo_orden[i]:
+                if nuevo_costo < costo_orden[i]: # Si el nuevo costo es menor se agrega y se actualiza el aterrizaje
                     costo_actual += nuevo_costo - costo_orden[i]
                     costo_orden[i] = nuevo_costo
                     tiempos_aterrizaje_actual[i] = nuevo_tiempo_aterrizaje
@@ -30,7 +30,7 @@ def hill_climbing(num_uavs: int, uavs: List[List[int]], t_espera: List[List[int]
                     tiempos_aterrizaje_actual[i] = nuevo_tiempo_aterrizaje
         elif tiempos_aterrizaje_actual[i] > uavs[i][1]:
             # Se ve si se puede mejorar el tiempo acercandolo a el tiempo preferente para bajar el costo
-            if n <= 0:
+            if n <= 0: # Si es mayor el tiempo de aterrizaje de la primera nave que el preferente puede tomar altiro el tiempo mas optimo
                 nuevo_tiempo_aterrizaje = uavs[i][1]
                 nuevo_costo = 0
                 if nuevo_costo < costo_orden[i]:
@@ -56,6 +56,7 @@ def hill_climbing(num_uavs: int, uavs: List[List[int]], t_espera: List[List[int]
     return costo_actual, tiempos_aterrizaje_actual, costo_orden
 
 
+# Hill climbing mejor mejora en este caso hace iteraciones del mismo algoritmo n veces hasta que no se hagan mas cambios que solucionen el costo
 def hill_climbing_mejor(num_uavs: int, uavs: List[List[int]], t_espera: List[List[int]], costo_actual, solucion_anterior, orden, costos) -> Tuple[int, List[int]]:
     tiempos_aterrizaje_actual = solucion_anterior.copy()
     costo_orden = costos.copy()
--- Tarea2/read.py
+++ Tarea2/read.py
@@ -26,40 +26,46 @@ def read_file(file_name: str) -> Tuple[int, List[List[int]], List[List[int]]]:
     return num_uavs, tiempos_aterrizaje, tiempos_requeridos
 
 
-archivo = 'C:/Users/Ketbome/Desktop/AlgoritmosExactosMetaheuristica/Tarea2/t2_Europa.txt'
+archivo = 'C:/Users/Ketbome/Desktop/AlgoritmosExactosMetaheuristica/Tarea2/t2_Titan.txt'
 
 num_uavs, uavs, t_espera = read_file(archivo)
 costo_determinista, tiempos_aterrizaje_determinista, orden, orden_costos = greedy_determinista(
     num_uavs, uavs, t_espera)
 print(f"Archivo: {archivo}")
-print(f"Greedy Determinista - Costo: {costo_determinista}")
-print(f"Tiempos de aterrizaje: {tiempos_aterrizaje_determinista}")
+print(f"Greedy Determinista - Costo Total: {costo_determinista}")
+print(f"Greedy Determinista - Costo en la nave i: {orden_costos}")
+print(f"Tiempos de aterrizaje de cada nave i: {tiempos_aterrizaje_determinista}")
 print("Hill Climbing - Greedy determinista:")
 costo_hill_climbing, tiempos_aterrizaje_hill_climbing, orden_costos_hill = hill_climbing(
     num_uavs, uavs, t_espera, costo_determinista, tiempos_aterrizaje_determinista, orden, orden_costos)
-print(f"  Seed {0} - Costo: {costo_hill_climbing}")
-print(f"  Tiempos de aterrizaje: {tiempos_aterrizaje_hill_climbing}")
+print(f"  Costo Total: {costo_hill_climbing}")
+print(f"  Costo en la nave i: {orden_costos_hill}")
+print(f"  Tiempos de aterrizaje de cada nave i: {tiempos_aterrizaje_hill_climbing}")
 print("Hill Climbing Mejor Mejora- Greedy determinista:")
 costo_hill_climbing, tiempos_aterrizaje_hill_climbing, orden_costos_hill_mejor = hill_climbing_mejor(
     num_uavs, uavs, t_espera, costo_determinista, tiempos_aterrizaje_determinista, orden, orden_costos)
-print(f"  Seed {0} - Costo: {costo_hill_climbing}")
-print(f"  Tiempos de aterrizaje: {tiempos_aterrizaje_hill_climbing}")
+print(f"  Costo Total: {costo_hill_climbing}")
+print(f"  Costo en la nave i: {orden_costos_hill_mejor}")
+print(f"  Tiempos de aterrizaje de cada nave i: {tiempos_aterrizaje_hill_climbing}")
 print()
 for seed in range(5):
     print(f"Greedy Estocástico seed {seed}:")
     costo_estocastico, tiempos_aterrizaje_estocastico, orden, orden_costos = greedy_estocastico(
         num_uavs, uavs, t_espera, seed)
-    print(f"  Seed {seed} - Costo: {costo_estocastico}")
-    print(f"  Tiempos de aterrizaje: {tiempos_aterrizaje_estocastico}")
+    print(f"  Seed {seed} - Costo Total: {costo_estocastico}")
+    print(f"  Costo en la nave i: {orden_costos}")
+    print(f"  Tiempos de aterrizaje de cada nave i: {tiempos_aterrizaje_estocastico}")
     print(f"  Hill Climbing - Greedy estocastico:")
     costo_hill_climbing, tiempos_aterrizaje_hill_climbing, orden_costos_hill = hill_climbing(
         num_uavs, uavs, t_espera, costo_estocastico, tiempos_aterrizaje_estocastico, orden, orden_costos)
-    print(f"     Seed {seed} - Costo: {costo_hill_climbing}")
-    print(f"     Tiempos de aterrizaje: {tiempos_aterrizaje_hill_climbing}")
+    print(f"     Seed {seed} - Costo Total: {costo_hill_climbing}")
+    print(f"     Costo en la nave i: {orden_costos_hill}")
+    print(f"     Tiempos de aterrizaje de cada nave i: {tiempos_aterrizaje_hill_climbing}")
     print(f"  Hill Climbing Mejor Mejora- Greedy estocastico:")
     costo_hill_climbing_mejor, tiempos_aterrizaje_hill_climbing_mejor, orden_costos_hill_mejor = hill_climbing_mejor(
         num_uavs, uavs, t_espera, costo_estocastico, tiempos_aterrizaje_estocastico, orden, orden_costos)
-    print(f"     Seed {seed} - Costo: {costo_hill_climbing_mejor}")
-    print(f"     Tiempos de aterrizaje: {tiempos_aterrizaje_hill_climbing_mejor}")
+    print(f"     Seed {seed} - Costo Total: {costo_hill_climbing}")
+    print(f"     Costo en la nave i: {orden_costos_hill}")
+    print(f"     Tiempos de aterrizaje de cada nave i: {tiempos_aterrizaje_hill_climbing}")
     print()
 print()
