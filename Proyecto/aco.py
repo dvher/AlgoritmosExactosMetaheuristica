b51fcadbc248f767e94e6284a571d95a332ed064
Ketbome <pims.2711@gmail.com>
2023-06-28 13:45:38 -0400

    fix: :bug: Mejora en algoritmo ACO

--- Proyecto/aco.py
+++ Proyecto/aco.py
@@ -8,11 +8,29 @@ class Ant:
         self.path = [start]
         self.visited = set([start])
         self.total_distance = 0
+        self.available_moves = []
 
     def move_to(self, position: Tuple[int, int], distance: int):
         self.path.append(position)
         self.visited.add(position)
         self.total_distance += distance
+
+    def update_available_moves(self, matrix: List[List[int]]):
+        current = self.path[-1]
+        neighbours = get_neighbours(matrix, current)
+        self.available_moves = []
+
+        for neighbour in neighbours:
+            if neighbour not in self.visited:
+                self.available_moves.append(neighbour)
+
+    def backtrack(self):
+        if len(self.path) >= 2:
+            self.total_distance -= calculate_distance(self.path[-1], self.path[-2])
+        self.path.pop()
+
+
+
 
 
 def calculate_distance(start: Tuple[int, int], end: Tuple[int, int]) -> int:
@@ -26,6 +44,7 @@ def get_neighbours(matrix: List[List[int]], pos: Tuple[int, int]) -> List[Tuple[
     cols = len(matrix[0]) if rows else 0
     neighbours = [(pos[0] + dx, pos[1] + dy) for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]]
     return [(nx, ny) for nx, ny in neighbours if 0 <= nx < rows and 0 <= ny < cols and matrix[nx][ny] != MATRIX_WALL]
+
 
 
 def aco(matrix: List[List[int]], start: Tuple[int, int], end: Tuple[int, int], num_ants: int, evaporation: float,
@@ -43,34 +62,38 @@ def aco(matrix: List[List[int]], start: Tuple[int, int], end: Tuple[int, int], n
 
         for ant in ants:
             while ant.path[-1] != end:
+                ant.update_available_moves(matrix)
                 current = ant.path[-1]
-                neighbours = get_neighbours(matrix, current)
 
-                probabilities = []
-                total = 0.0
+                if not ant.available_moves:
+                    ant.backtrack()
+                else:
+                    probabilities = []
+                    total = 0.0
 
-                for neighbour in neighbours:
-                    distance = calculate_distance(current, neighbour)
-                    pheromone = pheromone_matrix[neighbour[0]][neighbour[1]] ** alpha
-                    attractiveness = (1.0 / distance) ** beta
-                    total += pheromone * attractiveness
-                    probabilities.append((neighbour, pheromone * attractiveness))
+                    for neighbour in ant.available_moves:
+                        distance = calculate_distance(current, neighbour)
+                        pheromone = pheromone_matrix[neighbour[0]][neighbour[1]] ** alpha
+                        attractiveness = (1.0 / distance) ** beta
+                        total += pheromone * attractiveness
+                        probabilities.append((neighbour, pheromone * attractiveness))
 
-                probabilities = [(neighbour, prob / total) for neighbour, prob in probabilities]
+                    probabilities = [(neighbour, prob / total) for neighbour, prob in probabilities]
 
-                selected = None
+                    selected = None
 
-                while not selected:
-                    rand = random()
-                    cumulative_prob = 0.0
+                    while not selected:
+                        rand = random()
+                        cumulative_prob = 0.0
 
-                    for neighbour, prob in probabilities:
-                        cumulative_prob += prob
-                        if cumulative_prob >= rand:
-                            selected = neighbour
-                            break
+                        for neighbour, prob in probabilities:
+                            cumulative_prob += prob
+                            if cumulative_prob >= rand:
+                                selected = neighbour
+                                break
 
-                ant.move_to(selected, calculate_distance(current, selected))
+                    ant.move_to(selected, calculate_distance(current, selected))
+                    ant.update_available_moves(matrix)
 
             if ant.total_distance < best_distance:
                 best_path = ant.path
